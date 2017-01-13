89077fdea95bd9e2f30eb695295199c74d42918e
Rejitha A R <rejitha.ar@experionglobal.com>
2017-01-13 09:19:48 +0530

    Modified files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DataFilterComponent } from './data-filter.component';
+import {DataFilterService} from './data-filter.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFilterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [DataFilterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
